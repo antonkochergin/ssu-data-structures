7338079982c514f48e69516c4aa739e0f7db714e
antonkochergin <antonkochergin4@gmail.com>
2026-02-17 08:17:09 +0400

    Вернул сортировку подсчетом к обычному виду без отрицательных

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -1,53 +1,36 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-void countingSort(int a[], int N, int mx)
+void countingSort(int a[], int N, int k)
 {
-    // Находим минимальное значение в массиве
-    int mn = a[0];
-    for (int i = 1; i < N; i++)
-    {
-        if (a[i] < mn)
-            mn = a[i];
-    }
 
-    // Корректируем размер массива с учетом отрицательных
-    int range = mx - mn + 1;
-    int *b = new int[range]();
+    int *b = new int[k]();
 
-    // Подсчет элементов со смещением на minVal
     for (int i = 0; i < N; i++)
     {
-        b[a[i] - mn]++;
+        b[a[i]]++;
     }
 
-    // Вывод с обратным смещением
-    for (int i = 0; i < range; i++)
+    for (int i = 0; i < k; i++)
     {
         for (int j = 0; j < b[i]; j++)
         {
-            cout << i + mn << " ";
+            cout << i << " ";
         }
     }
 
-    delete[] b;
+    delete[] b; // освобождаем память
 }
 
 int main()
 {
-    // Пример с отрицательными числами
-    int a[] = {-5, 3, -2, 4, -8, 1, -3, 4, -2, 7};
+    int a[] = {4, 2, 2, 8, 3, 3, 1, 4, 2, 7};
     int n = sizeof(a) / sizeof(a[0]);
-    int mx = 0;
+    int k = 9;
 
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] > mx)
-            mx = a[i];
-    }
-
-    countingSort(a, n, mx);
+    cout << "Отсортированный массив: ";
+    countingSort(a, n, k);
     cout << endl;
 
     return 0;
